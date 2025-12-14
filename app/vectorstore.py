c6173c83c23cbceb078ca7d008deee9cb61e9aab
bradley3322 <bradleyrobinson332@gmail.com>
2025-12-14 13:43:09 +0000

    Add upserting collection data to reduce dublicate data, add delete collection and delete collection data for future fastAPI implimentation

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,27 +1,34 @@
 import chromadb
-chroma_client = chromadb.chromadb.PersistentClient()
 import logging
+chroma_client = chromadb.chromadb.PersistentClient()
+collection = chroma_client.get_or_create_collection(name="test_collection")
+
 
 def add_chunks_to_db(id_list, documents_list, metadata_list, embeddings_list):
     logging.info("Adding chunks to ChromaDB.")
-    collection = chroma_client.get_or_create_collection(name="test_collection")
-    collection.add(
+    collection.upsert(
         ids=id_list,
         documents=documents_list,
         metadatas=metadata_list,
         embeddings=embeddings_list)
     print(documents_list)
+        
+def delete_collection(collection_name):
+    logging.info("deleting collection: " + collection_name)
+    chroma_client.delete_collection(name=collection_name)
+
+# TODO: Implement proper deletion of all data in the DB with POST FastAPI endpoint
+def delete_collection_data():
+    collection.delete(
+        ids=[],
+        where={},
+        )
+
     
-def delete_all_db_data():
-    logging.info("deleting all db data.")
-    collection = chroma_client.get_or_create_collection(name="test_collection")
-    collection.delete(where={'source_file': 'test_one'})
-    
-def query_texts_from_db(query_texts, n_results=1):
-    logging.info("Querying texts from ChromaDB: " + query_texts)
-    collection = chroma_client.get_or_create_collection(name="test_collection")
+def query_texts_from_db(query_text, n_results=1):
+    logging.info("Querying texts from ChromaDB: " + query_text)
     results = collection.query(
-        query_texts=[query_texts],
+        query_texts=[query_text],
         n_results=n_results
     )
     logging.info(f"Retrieved {len(results['ids'][0])} results from query.")
